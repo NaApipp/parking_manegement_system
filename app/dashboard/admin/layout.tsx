6157f3f4436ae2e5a3e10f10b0f19feda730450a
NabilApipp <nabilapipp@gmail.com>
2026-03-12 23:06:52 +0700

    chore: update path direct sidebar

--- app/dashboard/admin/layout.tsx
+++ app/dashboard/admin/layout.tsx
@@ -67,7 +67,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <ul className="mt-2 space-y-1 px-4">
               <li>
                 <Link
-                  href="/dashboard/admin/tambah-user"
+                  href="/dashboard/admin/parking-management/tarif-parkir"
                   className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-200"
                 >
                   Tarif Parkir
@@ -76,7 +76,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
               <li>
                 <Link
-                  href="/dashboard/admin/user-admin"
+                  href="/dashboard/admin/parking-management/area-parkir"
                   className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-200"
                 >
                   Area Parkir
@@ -84,7 +84,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
               </li>
               <li>
                 <Link
-                  href="/dashboard/admin/user-admin"
+                  href="/dashboard/admin/parking-management/kendaraan"
                   className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-200"
                 >
                   Kendaraan
