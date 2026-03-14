4137db80babc4431e5ef743d56d8b6dedd4a0a48
NabilApipp <nabilapipp@gmail.com>
2026-03-14 12:59:45 +0700

    chore: create logic for logActivity

---
+++ app/lib/logActivity.ts
@@ -0,0 +1,9 @@
+import db from "./db"
+
+export async function logActivity(id_user: number, aktivitas: string) {
+  await db.execute(
+    `INSERT INTO tb_log_aktivitas (id_user, aktivitas, waktu_aktivitas)
+     VALUES (?, ?, NOW())`,
+    [id_user, aktivitas]
+  )
+}
