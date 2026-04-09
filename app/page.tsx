721ed2a247500e4df13566f42a00445967a623c4
NabilApipp <nabilapipp@gmail.com>
2026-04-09 20:46:13 +0700

    chore: update main page redirect to /login

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import { redirect } from "next/navigation";
 import MaintenancePage from "./components/Maintenance";
 
 export default function Home() {
-    // redirect("/login");
-    return (
-        <>
-        <MaintenancePage />
-        </>
-    )
+    redirect("/login");
+    // return (
+    //     <>
+    //     <MaintenancePage />
+    //     </>
+    // )
 }
